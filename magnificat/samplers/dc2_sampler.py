b818a783470989779fd8551676a0b19cfbce7774
jiwoncpark <jiwon.christine.park@gmail.com>
2021-05-26 16:55:15 -0400

    Add an internal counter that increments each time sample is called

--- magnificat/samplers/dc2_sampler.py
+++ magnificat/samplers/dc2_sampler.py
@@ -5,17 +5,19 @@ import pandas as pd
 
 class DC2Sampler:
 
-    def __init__(self, seed=123, bandpasses=list('ugrizy')):
+    def __init__(self, seed=123, bandpasses=list('ugrizy'), idx=None):
         self.seed = seed
         self.rng = np.random.default_rng(self.seed)
         self.bandpasses = bandpasses
         self.zp = -2.5*np.log10(3631)
         self.to_Jy = 1.e-9  # nanoJy to Jy
+        self.idx = idx
         import magnificat.input_data as in_data
         self.in_data = in_data.__path__[0]
         self.flux_cols = [f'flux_{bp}' for bp in self.bandpasses]
         self.mag_cols = [f'mag_{bp}' for bp in self.bandpasses]
         self._load_catalog()
+        self.count = 0
 
     def _load_param_cat(self):
         df = pd.read_csv(os.path.join(self.in_data, 'joined_0.csv'))
@@ -52,18 +54,20 @@ class DC2Sampler:
         cat = params.merge(truth, how='inner', suffixes=['', '_duplicate'],
                            left_on='galaxy_id', right_on='host_galaxy',
                            validate='one_to_one')
-        print(cat.shape)
         master_cols = ['galaxy_id', 'redshift', 'BH_mass', 'M_i']  # 'accretion_rate', 'edd_ratio'
         master_cols += self.mag_cols
         master_cols += [f'SF_inf_{bp}' for bp in self.bandpasses]
         master_cols += [f'tau_{bp}' for bp in self.bandpasses]
-        self.cat = cat[master_cols].copy()
+        cat.reset_index(drop=True, inplace=True)
+        self.cat = cat[master_cols].iloc[self.idx].copy().reset_index(drop=True)
         self.cat['BH_mass'] = np.log10(self.cat['BH_mass'].values)
         self.size = self.cat.shape[0]
 
     def sample(self):
-        i = self.rng.integers(low=0, high=self.size)
-        return self.cat.iloc[i]
+        # i = self.rng.integers(low=0, high=self.size)
+        sample = self.cat.iloc[self.count]
+        self.count += 1
+        return sample
 
     def flux_to_mag(self, flux):
         mag = -2.5*np.log10(flux) - self.zp
@@ -71,7 +75,9 @@ class DC2Sampler:
 
 
 if __name__ == '__main__':
-    sampler = DC2Sampler(123, ['i'])
+    sampler = DC2Sampler(123, ['i'], idx=[5, 6, 8])
+    params = sampler.sample()
+    print(params)
     params = sampler.sample()
     print(params)
     assert params['redshift'].dtype == np.float64
