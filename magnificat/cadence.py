1ce8ff3553d3b00fce1a852cac52aeddda287fb7
Ji Won Park <jiwoncpark@gmail.com>
2021-08-03 18:18:24 -0700

    Trim unobserved times, tests passing

--- magnificat/cadence.py
+++ magnificat/cadence.py
@@ -33,12 +33,7 @@ class LSSTCadence:
             hp_list = [int(line.rstrip()) for line in f.readlines()]
         self.hp_list = hp_list
 
-    def set_bandpasses(self, bandpasses):
-        self.bandpasses_int = [self.bp_to_int[bp] for bp in bandpasses]
-        self.bandpasses_int.sort()
-        self.bandpasses = [self.int_to_bp[bp_i] for bp_i in self.bandpasses_int]
-
-    def get_pointings_single_hp(self, hp: int, n_pointings: int):
+    def get_pointings_single_hp(self, hp: int, n_pointings_init: int):
         """
         Get pointing positions from a single healpix, upgrading it if necessary
 
@@ -46,7 +41,7 @@ class LSSTCadence:
         ----------
         hp : int
             a single healpix id in DC2 (NSIDE=32, nested)
-        n_pointings : int
+        n_pointings_init : int
             how many pointings to get from this healpix
 
         Returns
@@ -55,20 +50,20 @@ class LSSTCadence:
             ra, dec of pointings
 
         """
-        nside_out = obs_utils.get_target_nside(n_pointings, self.nside_in)
+        nside_out = obs_utils.get_target_nside(n_pointings_init, self.nside_in)
         # Upsampled healpix IDs in nested scheme
         hp_ids = obs_utils.upgrade_healpix(hp, self.nested,
                                            self.nside_in, nside_out)
         ra, dec = obs_utils.get_healpix_centers(hp_ids, nside_out, nest=True)
-        return ra[:n_pointings], dec[:n_pointings]
+        return ra[:n_pointings_init], dec[:n_pointings_init]
 
-    def get_pointings(self, n_pointings: int):
+    def get_pointings(self, n_pointings_init: int):
         """
         Get pointing positions all over the DC2 field
 
         Parameters
         ----------
-        n_pointings : int
+        n_pointings_init : int
             how many pointings to get
 
         Returns
@@ -77,53 +72,142 @@ class LSSTCadence:
             ra, dec of pointings
 
         """
-        pointings_per_hp = math.ceil(n_pointings/len(self.hp_list))
+        pointings_per_hp = math.ceil(n_pointings_init/len(self.hp_list))
         ra = np.empty(pointings_per_hp*len(self.hp_list))
         dec = np.empty(pointings_per_hp*len(self.hp_list))
         for i, hp in enumerate(self.hp_list):
             r, d = self.get_pointings_single_hp(hp, pointings_per_hp)
             ra[i*pointings_per_hp:(i+1)*pointings_per_hp] = r
             dec[i*pointings_per_hp:(i+1)*pointings_per_hp] = d
-        return ra[:n_pointings], dec[:n_pointings]
+        return ra[:n_pointings_init], dec[:n_pointings_init]
 
-    def get_obs_info(self, ra: np.ndarray, dec: np.ndarray, skip_existing=True):
+    def get_obs_info(self, ra: np.ndarray, dec: np.ndarray,
+                     skip_existing=True, skip_ddf=True):
+        """Loop through pointings and query visits that fall inside FOV
+
+        Note
+        ----
+        After DDF rejection, we might end up with a final `n_pointings`
+        different from `n_pointings_init`
+
+        """
         opsim = self.load_opsim_db()
+        n_pointings = 0  # init
         for i, (r, d) in tqdm(enumerate(zip(ra, dec)), total=len(ra)):
             if skip_existing:
                 if os.path.exists(osp.join(self.out_dir, f'mask_{i}.npy')):
                     continue
             obs_info_i = pd.DataFrame()
+            # Get distance from each visit row to pointing, (r, d)
             dist, _, _ = obs_utils.get_distance(ra_f=opsim['ra'].values,
                                                 dec_f=opsim['dec'].values,
                                                 ra_i=r,
                                                 dec_i=d)
-            opsim['dist'] = dist  # deg
-            obs_info_i = obs_info_i.append(opsim[opsim['dist'] < self.fov_radius],
+            opsim['dist'] = dist  # deg, include in obs_info_i to store
+            # Misc obs info
+            obs_info_i = obs_info_i.append(opsim[dist < self.fov_radius],
                                            ignore_index=True)
-            obs_info_i.to_csv(osp.join(self.out_dir, f'obs_{i}.csv'), index=None)
             # Get filter assignment in int, for convenience
             filters = obs_info_i['filter'].values
             filters = np.array(list(map(self.bp_to_int.get, filters)))
-            # Store MJD
+            # Store MJD for easy access
             mjd = (obs_info_i['expMJD'].values - self.min_mjd)
             mjd.sort()
-            np.save(osp.join(self.out_dir, f'mjd_{i}.npy'), mjd)
+            # Skip DDF visits, which have more than 1500 visits
+            if skip_ddf:
+                if len(mjd) > 1500:
+                    continue
             # Store observation mask, 1 where observed in filter else 0
             mask = np.zeros([len(mjd), 6]).astype(bool)  # [n_obs, 6]
             for bp_i in range(6):
                 mask[:, bp_i] = (filters == bp_i)
-            np.save(osp.join(self.out_dir, f'mask_{i}.npy'), mask)
+            # Store obs info, MJD, and mask for later access
+            obs_info_i.to_csv(osp.join(self.out_dir,
+                                       f'obs_{n_pointings}.csv'),
+                              index=None)
+            np.save(osp.join(self.out_dir,
+                             f'mjd_{n_pointings}.npy'), mjd)
+            np.save(osp.join(self.out_dir,
+                             f'mask_{n_pointings}.npy'), mask)
+            n_pointings += 1
+        self.n_pointings = n_pointings
+
+    def bin_by_day(self, skip_existing=True):
+        """Bin the observations by day
+
+        Parameters
+        ----------
+        skip_existing : bool, optional
+            whether to skip operations for pointings already saved
+            to disk
+
+        """
+        # Mask indicating if a time was observed at least once in any filter
+        full_mjd = np.arange(3650)
+        observed = np.zeros(3650).astype(bool)  # init as all unobserved
+        # Iterate through all pointings to identify days to trim
+        for p in range(self.n_pointings):
+            mjd = self.get_mjd_single_pointing(p, rounded=True)  # [n_obs,]
+            observed[mjd] = True  # mask observed
+        np.save(osp.join(self.out_dir,
+                         'observed_mask.npy'), observed)
+        mjd_trimmed = full_mjd[observed]  # [trimmed_T,]
+        print(f"Trimmed MJD has {len(mjd_trimmed)} out of 3650 days.")
+        np.save(osp.join(self.out_dir,
+                         'mjd_trimmed.npy'), mjd_trimmed)
+        # Compile binned mask
+        for p in range(self.n_pointings):
+            if skip_existing:
+                if os.path.exists(osp.join(self.out_dir,
+                                           f'trimmed_mask_{p}.npy')):
+                    continue
+            # 10-year full mask in all filters
+            full = np.zeros([3650, 6]).astype(bool)  # init, all unobserved
+            # 10-year observed days combined across filters for this pointing
+            mjd = self.get_mjd_single_pointing(p, rounded=True)  # [n_obs,]
+            mask = self.get_mask_single_pointing(p)  # [n_obs, 6]
+            for bp_i in range(6):
+                full[mjd, bp_i] = mask[:, bp_i]
+            trimmed = full[observed, :]  # trim so only relevant times remain
+            np.save(osp.join(self.out_dir,
+                             f'trimmed_mask_{p}.npy'), trimmed)
+
+    def get_observed_mask(self):
+        """Get trimmed MJD that is observed in at least one band at all
+        times
+
+        """
+        return np.load(osp.join(self.out_dir, 'observed_mask.npy'))
+
+    def get_trimmed_mjd(self):
+        """Get trimmed MJD that is observed in at least one band at all
+        times
+
+        """
+        return np.load(osp.join(self.out_dir, 'mjd_trimmed.npy'))
+
+    def get_trimmed_mask(self, i: int):
+        """Get trimmed mask that has times corresponding to trimmed_mjd
+
+        """
+        mask = np.load(osp.join(self.out_dir, f'trimmed_mask_{i}.npy'))
+        mask = mask.astype(bool)
+        return mask
 
     def get_mjd_single_pointing(self, i: int, rounded: bool):
         mjd = np.load(osp.join(self.out_dir, f'mjd_{i}.npy'))
         if rounded:
-            mjd = np.round(mjd)
+            mjd = np.round(mjd).astype(np.int64)
         return mjd
 
+    def get_mjd_i_band_pointing(self, i: int, rounded: bool):
+        mjd = np.load(osp.join(self.out_dir, f'mjd_{i}.npy'))
+        mask = np.load(osp.join(self.out_dir, f'mask_{i}.npy')).astype(bool)
+        i_band_int = mjd[mask[:, 3]]
+        return i_band_int
+
     def get_mask_single_pointing(self, i: int):
-        mask = np.load(osp.join(self.out_dir, f'mask_{i}.npy'))
-        if self.bandpasses_int is not None:
-            mask = mask[:, self.bandpasses_int]
+        mask = np.load(osp.join(self.out_dir, f'mask_{i}.npy')).astype(bool)
         return mask
 
     def load_opsim_db(self):
@@ -141,12 +225,12 @@ class LSSTCadence:
 
 if __name__ == '__main__':
     cadence_obj = LSSTCadence('obs', 1234)
-    ra, dec = cadence_obj.get_pointings(10)
+    ra, dec = cadence_obj.get_pointings(100)
     cadence_obj.get_obs_info(ra, dec)
-    cadence_obj.set_bandpasses(['i', 'g'])
     mjd = cadence_obj.get_mjd_single_pointing(0, rounded=False)
     print("mjd: ", mjd.shape)
     mask = cadence_obj.get_mask_single_pointing(0)
     print("mask: ", mask.shape)
     print(ra.shape)
+    cadence_obj.bin_by_day()
 
---
+++ tests/test_cadence.py
@@ -0,0 +1,86 @@
+import unittest
+import shutil
+import numpy as np
+from magnificat import cadence
+
+
+class TestLSSTCadence(unittest.TestCase):
+    """Tests for LSSTCadence class
+
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.out_dir = 'obs_testing'
+
+    def test_get_pointings(self):
+        """Test input and output shapes of get_pointings
+        """
+        cadence_obj = cadence.LSSTCadence(self.out_dir, 1234)
+        ra, dec = cadence_obj.get_pointings(100)
+        np.testing.assert_equal(len(ra), 100)
+        np.testing.assert_equal(len(dec), 100)
+
+    def test_get_obs_info(self):
+        """Test queried visits of get_obs_info
+
+        """
+        cadence_obj = cadence.LSSTCadence(self.out_dir, 1234)
+        ra, dec = cadence_obj.get_pointings(100)
+        cadence_obj.get_obs_info(ra, dec, skip_ddf=True)
+        # Final n_pointings should be <= requested 100
+        # if there were DDF pointings
+        assert cadence_obj.n_pointings <= 100
+        for p in range(cadence_obj.n_pointings):
+            # Number of visits should be < 1500 if DDF was skipped
+            mjd = cadence_obj.get_mjd_single_pointing(p, rounded=False)
+            assert len(mjd) < 1500
+
+    def test_get_mjd_single_pointing(self):
+        """Test `get_mjd_single_pointing`
+
+        """
+        cadence_obj = cadence.LSSTCadence(self.out_dir, 1234)
+        ra, dec = cadence_obj.get_pointings(100)
+        cadence_obj.get_obs_info(ra, dec, skip_ddf=True)
+        mjd = cadence_obj.get_mjd_single_pointing(0, rounded=True)
+        assert mjd.dtype is np.dtype(np.int64)
+        assert np.min(mjd) >= 0
+        assert np.max(mjd) <= 3650
+
+    def test_get_mask_single_pointing(self):
+        """Test `get_mask_single_pointing`
+
+        """
+        cadence_obj = cadence.LSSTCadence(self.out_dir, 1234)
+        ra, dec = cadence_obj.get_pointings(100)
+        cadence_obj.get_obs_info(ra, dec, skip_ddf=True)
+        mjd = cadence_obj.get_mjd_single_pointing(0, rounded=True)
+        T = len(mjd)
+        mask = cadence_obj.get_mask_single_pointing(0)
+        np.testing.assert_equal(mask.shape, [T, 6])
+        assert mask.dtype is np.dtype(bool)
+
+    def test_bin_by_day(self):
+        """Test `bin_by_day`
+
+        """
+        cadence_obj = cadence.LSSTCadence(self.out_dir, 1234)
+        ra, dec = cadence_obj.get_pointings(100)
+        cadence_obj.get_obs_info(ra, dec, skip_ddf=True)
+        cadence_obj.bin_by_day()
+        obs_mask = cadence_obj.get_observed_mask()
+        assert obs_mask.dtype is np.dtype(bool)
+        trimmed_mjd = cadence_obj.get_trimmed_mjd()
+        trimmed_T = len(trimmed_mjd)
+        assert trimmed_T == sum(obs_mask)
+        trimmed_mask = cadence_obj.get_trimmed_mask(0)
+        np.testing.assert_equal(trimmed_mask.shape, [trimmed_T, 6])
+        assert trimmed_mask.dtype is np.dtype(bool)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.out_dir)
+
+
+if __name__ == '__main__':
+    unittest.main()
