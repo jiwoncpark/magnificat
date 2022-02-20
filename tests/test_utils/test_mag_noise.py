dc6ac98ce1a66c4e4cc32e4548436541633b316e
Ji Won Park <jiwoncpark@gmail.com>
2022-02-20 15:13:07 -0800

    Add mag-dependent mag noise module and test

---
+++ tests/test_utils/test_mag_noise.py
@@ -0,0 +1,41 @@
+import unittest
+import numpy as np
+import torch
+from magnificat.utils.mag_noise import MagNoise, MagNoiseTorch
+
+
+class TestMagNoise(unittest.TestCase):
+    """Tests for `drw_utils.py`
+
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.seed = 123
+        cls.mags_np = np.random.randn(100, 5)*1 + 22  # random simulated mags
+        cls.mags_torch = torch.randn(100, 5)*1 + 22  # random simulated mags
+
+    def test_mag_noise_np_ugriz(self):
+        """Check if noised mags are within reasonable range"""
+        mag_noise_np = MagNoise(mag_idx=[0, 1, 2, 3, 4],
+                                which_bands=list('ugriz'),
+                                override_kwargs=None,
+                                depth=10,
+                                airmass=1.15304)
+        noised = mag_noise_np(self.mags_np)
+        np.testing.assert_array_less(noised, np.ones_like(noised)*25)
+        np.testing.assert_array_less(np.ones_like(noised)*18, noised)
+
+    def test_mag_noise_torch_ugriz(self):
+        """Check if noised mags are within reasonable range"""
+        mag_noise_torch = MagNoiseTorch(mag_idx=[0, 1, 2, 3, 4],
+                                        which_bands=list('ugriz'),
+                                        override_kwargs=None,
+                                        depth=10,
+                                        airmass=1.15304)
+        noised = mag_noise_torch(self.mags_torch)
+        np.testing.assert_array_less(noised, np.ones_like(noised)*25)
+        np.testing.assert_array_less(np.ones_like(noised)*18, noised)
+
+
+if __name__ == '__main__':
+    unittest.main()
